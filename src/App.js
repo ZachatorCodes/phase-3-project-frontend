9e99f307d4d6d12e159daf472185fc5c4854ad9a
Zach Schwartz <zachatorcodes@gmail.com>
2023-06-14 12:20:01 -0600

    Set up basic fetch and state variables with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [trails, setTrails] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/trails")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTrails(data);
+      });
+  }, []);
 }
 
 export default App;
