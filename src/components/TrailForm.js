d874338dd48ba9612f0314cb4d6ee0712ac6a794
Zach Schwartz <zachatorcodes@gmail.com>
2023-06-15 17:00:04 -0600

    Built out basic form and basic event handler functions

--- src/components/TrailForm.js
+++ src/components/TrailForm.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TrailForm() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+  }
+
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
+
   return (
     <div className="TrailForm">
-      <h1>TrailForm</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Trail Name"
+          name="trail_name"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="Length"
+          name="length"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="Elevation Gain"
+          name="elevation"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="Location"
+          name="location"
+          onChange={handleChange}
+        ></input>
+        <select name="difficulty" onChange={handleChange}>
+          <option value="Easy">Easy</option>
+          <option value="Medium">Medium</option>
+          <option value="Hard">Hard</option>
+        </select>
+        <button type="submit">Add Trail</button>
+      </form>
     </div>
   );
 }
