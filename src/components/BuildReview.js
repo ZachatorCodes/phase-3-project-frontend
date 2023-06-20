7c4c753db405a8eae5c4a007b6c5523a3856f1b9
Zach Schwartz <zachatorcodes@gmail.com>
2023-06-20 15:54:24 -0600

    Finished building review cards

--- src/components/BuildReview.js
+++ src/components/BuildReview.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function BuildReview() {
+function BuildReview({ reviewID, comment, rating }) {
   return (
     <div className="Review">
-      <h1>Review</h1>
+      <h1>{`Rating: ${rating}/5`}</h1>
+      <h2>Trail Comment:</h2>
+      <h3>{comment}</h3>
     </div>
-  )
+  );
 }
 
 export default BuildReview;
