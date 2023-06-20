31bafd1b3e39c68515c0d9a21c82e13a85c6a2ff
Zach Schwartz <zachatorcodes@gmail.com>
2023-06-20 16:09:50 -0600

    Created basic UpdateTrail component

---
+++ src/components/UpdateTrail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UpdateTrail() {
+  return (
+    <div className="UpdateTrail">
+      <h1>UpdateTrail</h1>
+    </div>
+  );
+}
+
+export default UpdateTrail;
