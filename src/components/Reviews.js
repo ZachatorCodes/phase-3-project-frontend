dcd2d715701ab6a743e248bebc321a42bbe2ed3a
Zach Schwartz <zachatorcodes@gmail.com>
2023-06-21 20:52:24 -0600

    Passed handler function as prop to ReviewForm

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,14 +3,14 @@ import { useParams } from "react-router-dom";
 import BuildReview from "./BuildReview";
 import ReviewForm from "./ReviewForm";
 
-function Reviews({ trails }) {
+function Reviews({ trails, onAddReview }) {
   const { id } = useParams();
   const specificTrail = trails.find((trail) => trail.id === parseInt(id));
   return (
     <>
       <h1 className="Header">Reviews for {specificTrail.trail_name}</h1>
-      <h3>Add a new review</h3>
-      <ReviewForm />
+      <h3 className="CenterText">Add a new review</h3>
+      <ReviewForm trailID={specificTrail.id} onAddReview={onAddReview}/>
       <div className="Reviews">
         {specificTrail.reviews.map((review) => {
           console.log(review);
